e253284f657cc525770a73f621c052ba69220932
yuuuuuuan <1395131175@qq.com>
2023-12-31 02:59:06 +0800

    add

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,6 +1,7 @@
 use super::*;
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok, BoundedVec};
+use frame_system::AccountId;
 
 #[test]
 fn create_claim_works() {
@@ -58,6 +59,41 @@ fn revoke_claim_failed_with_wrong_owner() {
 
         assert_noop!(
             PoeModule::revoke_claim(RuntimeOrigin::signed(2), claim.clone()),
+            Error::<Test>::NotClaimOwner
+        );
+    })
+}
+
+#[test]
+fn transfer_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = BoundedVec::try_from(vec![0, 1]).unwrap();
+
+        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone());
+
+        assert_ok!(PoeModule::transfer_claim(RuntimeOrigin::signed(1), claim.clone(), AccountId::new("receiver")));
+    })
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_is_not_exist() {
+    new_test_ext().execute_with(|| {
+        let claim = BoundedVec::try_from(vec![0, 1]).unwrap();
+
+        assert_noop!(
+            PoeModule::transfer_claim(RuntimeOrigin::signed(1), claim.clone(), AccountId::new("receiver")),
+            Error::<Test>::ClaimNotExist
+        );
+    })
+}
+
+#[test]
+fn transfer_claim_failed_when_not_claim_owner() {
+    new_test_ext().execute_with(|| {
+        let claim = BoundedVec::try_from(vec![0, 1]).unwrap();
+        let _ = PoeModule::create_claim(RuntimeOrigin::signed(1), claim.clone());
+        assert_noop!(
+            PoeModule::transfer_claim(RuntimeOrigin::signed(1), claim.clone(), AccountId::new("1")),
             Error::<Test>::NotClaimOwner
         );
     })
